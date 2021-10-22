4a305d78c4988d3c4619658ebb7434cc43111c5f
JaraCalle <jaracalle2041@gmail.com>
2021-10-22 11:10:38 -0500

    Cambios al home

--- VAMOS.Android/obj/Debug/110/android/environment.x86.s
+++ VAMOS.Android/obj/Debug/110/android/environment.x86.s
@@ -71,7 +71,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"879d6f78-7750-4221-8491-f472f4fac236"
+	.asciz	"c8ae0374-d000-49f0-8901-8b717cd312dd"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
